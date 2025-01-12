b90e5a065f836d15d82813f3bf53afe4be3055d7
FlorescuMiruna <mirunastefaniaflorescu@gmail.com>
2025-01-12 16:49:27 +0200

    Added movie view

--- MovieApp/cinema-ticketing/app.py
+++ MovieApp/cinema-ticketing/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 import psycopg2
 from psycopg2 import sql
 
@@ -66,111 +66,28 @@ def get_db_connection():
     )
     return conn
 # GET: Pentru a obține toate filmele
-@app.route('/movies', methods=['GET'])
+# @app.route('/movies', methods=['GET'])
+# def get_movies():
+#     conn = get_db_connection()
+#     cur = conn.cursor()
+#     cur.execute("SELECT * FROM movies;")
+#     movies = cur.fetchall()
+#     cur.close()
+#     conn.close()
+#     return jsonify(movies)
+
+# GET: Pentru a obține toate filmele
+@app.route('/movies')
 def get_movies():
     conn = get_db_connection()
     cur = conn.cursor()
-    cur.execute("SELECT * FROM movies;")
+    cur.execute("SELECT * FROM movies;")  # Aici selectăm toate filmele
     movies = cur.fetchall()
     cur.close()
     conn.close()
+    return render_template('movies.html', movies=movies)
 
-    # Returnează filmele ca JSON
-    return jsonify(movies)
 
-# # GET: Pentru a obține un film după ID
-# @app.route('/movies/<int:movie_id>', methods=['GET'])
-# def get_movie(movie_id):
-#     conn = get_db_connection()
-#     cur = conn.cursor()
-#     cur.execute("SELECT * FROM movies WHERE id = %s;", (movie_id,))
-#     movie = cur.fetchone()
-#     cur.close()
-#     conn.close()
-#
-#     if movie:
-#         return jsonify(movie)
-#     else:
-#         return jsonify({"error": "Movie not found"}), 404
-#
-# # POST: Pentru a adăuga un film nou
-# @app.route('/movies', methods=['POST'])
-# def post_movie():
-#     new_movie = request.get_json()
-#
-#     title = new_movie['title']
-#     director = new_movie['director']
-#     year = new_movie['year']
-#
-#     conn = get_db_connection()
-#     cur = conn.cursor()
-#     cur.execute(
-#         "INSERT INTO movies (title, director, year) VALUES (%s, %s, %s) RETURNING id;",
-#         (title, director, year)
-#     )
-#     new_movie_id = cur.fetchone()[0]
-#     conn.commit()
-#     cur.close()
-#     conn.close()
-#
-#     return jsonify({"id": new_movie_id, "title": title, "director": director, "year": year}), 201
-#
-# # PUT: Pentru a actualiza un film existent
-# @app.route('/movies/<int:movie_id>', methods=['PUT'])
-# def put_movie(movie_id):
-#     updated_movie = request.get_json()
-#
-#     title = updated_movie.get('title')
-#     director = updated_movie.get('director')
-#     year = updated_movie.get('year')
-#
-#     conn = get_db_connection()
-#     cur = conn.cursor()
-#
-#     # Verificăm dacă filmul există
-#     cur.execute("SELECT * FROM movies WHERE id = %s;", (movie_id,))
-#     movie = cur.fetchone()
-#
-#     if not movie:
-#         cur.close()
-#         conn.close()
-#         return jsonify({"error": "Movie not found"}), 404
-#
-#     # Actualizăm filmul
-#     update_query = sql.SQL(
-#         "UPDATE movies SET title = %s, director = %s, year = %s WHERE id = %s;"
-#     )
-#     cur.execute(update_query, (title, director, year, movie_id))
-#     conn.commit()
-#
-#     cur.close()
-#     conn.close()
-
-    return jsonify({"id": movie_id, "title": title, "director": director, "year": year}), 200
-
-# DELETE: Pentru a șterge un film după ID
-@app.route('/movies/<int:movie_id>', methods=['DELETE'])
-def delete_movie(movie_id):
-    conn = get_db_connection()
-    cur = conn.cursor()
-
-    # Verificăm dacă filmul există
-    cur.execute("SELECT * FROM movies WHERE id = %s;", (movie_id,))
-    movie = cur.fetchone()
-
-    if not movie:
-        cur.close()
-        conn.close()
-        return jsonify({"error": "Movie not found"}), 404
-
-    # Ștergem filmul
-    cur.execute("DELETE FROM movies WHERE id = %s;", (movie_id,))
-    conn.commit()
-
-    cur.close()
-    conn.close()
-
-    return jsonify({"message": "Movie deleted successfully"}), 200
 
 
 if __name__ == '__main__':
